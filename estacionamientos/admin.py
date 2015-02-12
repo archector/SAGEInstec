003eb115029312ea8e7e23516cd234588be01191
Daniel Pelayo <daniel.pelayo23@gmail.com>
2015-02-11 23:28:22 -0430

    solucionado problema con visualizacion de estacionamiento desde admin

--- estacionamientos/admin.py
+++ estacionamientos/admin.py
@@ -2,6 +2,6 @@
 from django.contrib import admin
 from estacionamientos.models import Estacionamiento, ReservasModel, Tarifa
 
-admin.site.register(Estacionamiento,list_display=('Tarifa',))
+admin.site.register(Estacionamiento,list_display=('Propietario','Nombre','Rif'))
 admin.site.register(Tarifa, list_display=('tipoTarifa',),search_fields=('Tarifa',))
 admin.site.register(ReservasModel)
