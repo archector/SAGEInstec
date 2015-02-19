d45937447fc29deef3dede4f6ece4fbeae29dad0
daniel <daniel.zeait@gmail.com>
2015-02-19 17:08:55 -0430

    Terminada vista de cancelación de reserva

---
+++ estacionamientos/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('estacionamientos', '0009_auto_20150219_0042'),
+        ('estacionamientos', '0010_reservasmodel_costo'),
+    ]
+
+    operations = [
+    ]
