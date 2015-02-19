6ebaafda52a52d9b9fa524fec24a07a462ccec03
daniel pelayo <daniel.pelayo23@gmail.com>
2015-02-19 15:55:38 -0430

    En esta rama probaremos algunas modificaciones nuevas

---
+++ estacionamientos/migrations/0012_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('estacionamientos', '0010_reservasmodel_costo'),
+        ('estacionamientos', '0011_reservasmodel_costo'),
+    ]
+
+    operations = [
+    ]
